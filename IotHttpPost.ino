b256debd7ecf3d814acb601f592de86620cb6f17
Nathan Truyts <78803504+Nathtruyts@users.noreply.github.com>
2022-11-06 23:17:38 -0300

    Add esp32 program

---
+++ IotHttpPost.ino
@@ -0,0 +1,58 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+  
+const char* ssid = "Vivo Fibra";
+const char* password =  "d12f14c31";
+  
+void setup() {
+  
+  Serial.begin(115200);
+  delay(4000);   //Delay needed before calling the WiFi.begin
+  
+  WiFi.begin(ssid, password); 
+  
+  while (WiFi.status() != WL_CONNECTED) { //Check for the connection
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+  
+  Serial.println("Connected to the WiFi network");
+  
+}
+  
+void loop() {
+  
+ if(WiFi.status()== WL_CONNECTED){   //Check WiFi connection status
+  
+   HTTPClient http;   
+  
+   http.begin("http://jsonplaceholder.typicode.com/Nathtruyts/maintest/posts");  //Specify destination for HTTP request
+   http.addHeader("Content-Type", "text/plain");             //Specify content-type header
+  
+   int httpResponseCode = http.POST("{\"api_key\":\"tPmAT5Ab3j7F9\",\"sensor\":\"BME280\",\"value1\":\"24.25\",\"value2\":\"49.54\",\"value3\":\"1005.14\"}");   //Send the actual POST request
+  
+   if(httpResponseCode>0){
+  
+    String response = http.getString();                       //Get the response to the request
+  
+    Serial.println(httpResponseCode);   //Print return code
+    Serial.println(response);           //Print request answer
+  
+   }else{
+  
+    Serial.print("Error on sending POST: ");
+    Serial.println(httpResponseCode);
+  
+   }
+  
+   http.end();  //Free resources
+  
+ }else{
+  
+    Serial.println("Error in WiFi connection");   
+  
+ }
+  
+  delay(10000);  //Send a request every 10 seconds
+  
+}
